6fa56ed0375ba109ee3381b8e79efef2a2793d8f
Adelia <adelgants@gmail.com>
2021-07-22 02:20:15 +0300

    add html & css joke-list

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <JokesList />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import JokesList from './components/JokesList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    JokesList
   }
 }
 </script>
 
 <style lang="scss">
+$screen-md: 768px;
+%transition {
+ transition: .4s ease-out;
+}
+$joke-color: #F9F7F7;
+$liked-color: #99DDCC;
+
+@mixin w-from($screen) {
+  @media (min-width: $screen) {
+    @content;
+  }
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
+
+.list {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+
+  &-joke {
+    display: flex;
+    justify-content: space-between;
+    gap: 20px;
+    width: 250px;
+    padding: 20px 10px;
+    margin: 30px 0;
+    border-radius: 5px;
+    box-shadow: 0 4px 16px 0 rgb(0 0 0 / 8%);
+    @extend %transition;
+    &:hover {
+      // box-shadow: 0 4px 16px 0 rgb(0 0 0 / 30%);
+    }
+    @include w-from($screen-md){
+      width: 550px;
+      padding: 20px 20px;
+    }
+    &-like {
+      cursor: pointer;
+      &:hover {
+      }
+    }
+  }
+}
+
+
 </style>
---
+++ src/components/JokesList.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul class="list">
+    <li class="list-joke" v-for="joke in jokes" :key=joke.id>
+      <p class="list-joke-text"> {{ joke }} </p>
+      <img src="../assets/like.svg" class="list-joke-like" />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'JokesList',
+  data () {
+    return {
+      jokes: ['Saying that Java is nice because it works on every OS is like saying that anal sex is nice because it works on every gender', 'шутка2', 'шутка3', 'шутка4'],
+      isLoading: false,
+      error: null
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
