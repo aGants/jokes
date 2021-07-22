110a55dc27c60afc6515ebb41918b9f1e97d4cd3
Adelia <adelgants@gmail.com>
2021-07-22 03:27:16 +0300

    add search & search style

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
+  <Search />
   <JokesList />
 </template>
 
 <script>
 import JokesList from './components/JokesList.vue'
+import Search from './components/Search.vue'
 
 export default {
   name: 'App',
   components: {
-    JokesList
+    JokesList,
+    Search
   }
 }
 </script>
@@ -18,8 +21,8 @@ $screen-md: 768px;
 %transition {
  transition: .4s ease-out;
 }
-$joke-color: #F9F7F7;
-$first-color: #99DDCC;
+$first-color: #F9F7F7;
+$second-color: #99DDCC;
 
 @mixin w-from($screen) {
   @media (min-width: $screen) {
@@ -30,6 +33,7 @@ $first-color: #99DDCC;
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 }
@@ -50,7 +54,7 @@ $first-color: #99DDCC;
     border-radius: 5px;
     box-shadow: 0 4px 16px 0 rgb(0 0 0 / 8%);
     @include w-from($screen-md){
-      width: 550px;
+      width: 600px;
       padding: 20px 20px;
     }
     &-like {
@@ -59,11 +63,31 @@ $first-color: #99DDCC;
       cursor: pointer;
       transition: .2s ease-out;
       &:hover {
-        fill: $first-color;
+        fill: $second-color;
       }
     }
   }
 }
 
+.search {
+  width: 1000px;
+  margin: 70px 0 20px;
+  text-align: center;
+  height: 43px;
+  &__input {
+    width: 70%;
+    padding: 12px;
+    background-color: $first-color;
+    border: 2px solid $first-color;
+    border-radius: 4px;
+    &:focus, &:active {
+      border: 2px solid $second-color;
+      outline: none;
+    }
+    &::placeholder {
+      color: rgba(0, 0, 0, 0.56);
+    }
+  }
+}
 
 </style>
---
+++ src/components/Search.vue
@@ -0,0 +1,23 @@
+<template>
+  <div className="search">
+    <input 
+      type="text"
+      placeholder="Введите слово" 
+      className="search__input"
+      />
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data () {
+    return {
+      value: ''
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
